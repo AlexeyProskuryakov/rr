b0382ba6a8231b35cdbab57a23cd8e238615d2ca
alesha <alexey.proskuryakov@gmail.com>
2015-11-03 13:50:49 +0300

    openshift 10

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 import os
 
+from server import app as application
+
 virtenv = os.path.join(os.environ.get('OPENSHIFT_PYTHON_DIR', '.'), 'virtenv')
 virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
 try:
@@ -9,7 +11,7 @@ except IOError:
     pass
 
 if __name__ == '__main__':
-    from server import app as application
+
     from wsgiref.simple_server import make_server
 
     httpd = make_server('localhost', 8051, application)
