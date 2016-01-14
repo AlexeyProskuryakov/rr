9bf01cf1127677d86b2d6d38a5a1a52de5c90c30
alesha <alexey.proskuryakov@gmail.com>
2016-01-14 23:22:52 +0300

    analysing 2 better

--- wsgi/bot/activity.py
+++ wsgi/bot/activity.py
@@ -1,10 +1,18 @@
 from collections import defaultdict
-from wsgi.engine import reddit_search, MAX_COUNT
+import time
+
+from wsgi.engine import MAX_COUNT, reddit, to_save, to_show
 
 
 def get_interested_posts(sbrdt):
-    interested = reddit_search("subreddit:\'%s\'" % sbrdt)
-    return interested
+    result = []
+    cur = int(time.time())
+    before = int(time.time()) - (3600 * 24 * 7 * 2)
+    for post in reddit.search("timestamp:%s..%s"%(before, cur), limit=MAX_COUNT, count=MAX_COUNT, subreddit=sbrdt, syntax="cloudsearch"):
+        post_info = to_save(to_show(post))
+        result.append(post_info)
+    return result
+
 
 
 def _get_coefficients(posts_clusters, n):
@@ -15,23 +23,30 @@ def _get_coefficients(posts_clusters, n):
 
 
 def evaluate_statistics(sbrdt_name):
+    """
+    Evaluating statistics for input subreddit
+
+    :param sbrdt_name:
+    :return: 2 dicts of weights and days and hours at day.
+    """
     posts = get_interested_posts(sbrdt_name)
 
     days = defaultdict(list)
     hours = defaultdict(list)
     for post in posts:
         days[post.get("created_dt").weekday()].append(post)
-        hours["%s_%s" % (post.get("created_dt").weekday(), post.get("created_dt").hour)].append(post)
+        hours[post.get("created_dt").hour].append(post)
 
     n = len(posts)
+    print "eval statistic for %s posts"%n
     return _get_coefficients(days, n), _get_coefficients(hours, n)
 
 
 if __name__ == '__main__':
-    days, hours = evaluate_statistics(sbrdt_name="woahdude")
+    days, hours = evaluate_statistics(sbrdt_name="cringe")
     print "\n".join(["%s : %s" % (k, v) for k, v in days.iteritems()])
 
     h_keys = hours.keys()
     h_keys.sort()
 
-    print "\n".join(["%s : %s" % (k, hours[k]) for k in h_keys])
+    # print "\n".join(["%s : %s" % (k, hours[k]) for k in h_keys])
